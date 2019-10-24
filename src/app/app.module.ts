87d538270742794af3b98a97c0553435f50e750f
AndriBunko <andr1bunk02@gmail.com>
2019-10-24 12:02:41 +0300

    section 4, lesson 30 creating directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {StyleDirective} from './directives/style.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StyleDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
